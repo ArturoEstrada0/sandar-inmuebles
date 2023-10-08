5f4526f1233bb168c41eb4d23b1a0d86ebae83df
Mitilene2001 <l20120152@morelia.tecnm.mx>
2023-10-08 16:07:38 -0600

    Mi primer commit

--- src/components/CountryDropdown.jsx
+++ src/components/CountryDropdown.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 
-function CountryDropdown() {
-  return (
-    <div><h1>Mi primer commit</h1></div>
-  )
+//import icons
+import {
+  RiMapPinLine, RiArrowDownSLine,
+  RiArrowUpLine
+} from 'react-icons/ri'
+
+//import headless ui
+import { Menu } from '@headlessui/react'
+
+//import house context
+import { HouseContext } from '.HouseContext'
+
+const CountryDropdown = () => {
+
+  const { country, setCountry, countries } =
+    useContext(HouseContext)
+
+    console.log(country)
+  return
+  <div>CountryDropdown</div>
+
 }
 
 export default CountryDropdown
