e76a7f025cf8426b22a9ac2fcaf78cc9748d4b92
Mitilene2001 <l20120152@morelia.tecnm.mx>
2023-10-09 11:16:28 -0600

    country drop de more

--- src/components/CountryDropdown.jsx
+++ src/components/CountryDropdown.jsx
@@ -17,10 +17,55 @@ const CountryDropdown = () => {
   const { country, setCountry, countries } =
     useContext(HouseContext)
 
-    console.log(country)
-  return
-  <div>CountryDropdown</div>
+  console.log(countries)
 
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <Menu as='div' className='dropdown relative'>
+
+      <Menu.Button onClick={() => setIsOpen(!isOpen)} className='dropdown-btn w-full'
+        text-left >
+
+        <RiMapPinLine className='dropdown-icon-primary' />
+
+        <div>
+
+          <div className='text-[15px] font-medium leading-tight' >
+            {country}</div>
+          <div className='text-[13px]'>Select your place</div>
+          {
+            isOpen ? (
+              <RiArrowUpSLine className='drop-icon-secondary' />
+            ) : (
+              <RiArrowDownSLine className='drop-icon-secondary' />
+            )
+          }
+
+        </div>
+
+
+      </Menu.Button>
+
+      <Menu.Items className='dropdown-menu'>
+        {countries.map((country, index) => {
+          return (
+            <Menu.Item
+              onClick={() => setCountry(country)}
+              className='cursor.pointer hover:text-violet-700 transition'
+              as='li'
+              key={index}>
+              {country}
+            </Menu.Item>
+          )
+        }
+
+
+        )}
+      </Menu.Items>
+
+    </Menu>
+  )
 }
 
 export default CountryDropdown
