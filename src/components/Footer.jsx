07a8a1a359d1ba276ed3efb077aca586b70268ce
ArturoEstrada0 <116518245+ArturoEstrada0@users.noreply.github.com>
2023-10-09 19:53:40 -0600

    implementacion completa

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 
 const Footer = () => {
-  return (
-    <>
-      <div>FOOTER</div>
-    </>
-  );
+  return <footer className="bg-black py-8 text-center text-white">
+    <div className="container mx-auto">Copyright &copy; 2023. All right reserved</div>
+  </footer>;
 };
 
 export default Footer;
